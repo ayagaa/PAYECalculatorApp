740ade98dcf18f8fe981e173f10ad997b5c137e6
ayagaa <odwar235@gmail.com>
2021-11-28 19:09:17 +0300

    Init commit

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#fff' }} />
+);
+
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#fff' }} />
+);
+
+const ThirdRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#fff' }} />
+);
+
+const FourthRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#fff' }} />
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+  third: ThirdRoute,
+  fourth: FourthRoute
+});
+
+
 
 export default function App() {
+
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'Profile' },
+    { key: 'second', title: 'Earnings' },
+    { key: 'third', title: 'Taxes' },
+    { key: 'fourth', title: 'Reports' }
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
   );
 }
 
